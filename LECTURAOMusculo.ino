0bb70b1da40dfe91123e1e1a8638481fadc3fb01
Bioelectronica Se escribe con B <35161121+BioelectronicaSeEscribeConB@users.noreply.github.com>
2018-08-14 00:09:03 -0500

    Add files via upload

---
+++ LECTURAOMusculo.ino
@@ -0,0 +1,30 @@
+int senalOjo=A0;
+int lecturaOjo=0;
+
+void setup() 
+{
+Serial.begin(9600);
+pinMode(senalOjo,INPUT);
+pinMode(9,OUTPUT);
+}
+
+void loop() 
+{
+lecturaOjo=analogRead(senalOjo);
+Serial.print(lecturaOjo);
+Serial.print(",");
+Serial.print(700);
+Serial.print(",");
+Serial.println(000);
+if(lecturaOjo>=750)
+{
+digitalWrite(9,HIGH);
+delay(10);
+}
+else
+{digitalWrite(9,LOW);
+delay(10);
+}
+
+delay(20);
+}
